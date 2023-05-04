136204b5f7bc91c175052500395f5d922ff57fb2
maysaa1992 <batainehmaysaa@gmail.com>
2023-05-04 18:58:33 +0000

    lab02

--- series_test/test_series.py
+++ series_test/test_series.py
@@ -13,63 +13,63 @@ n=5 -> '5'
 n=6 -> '8'
 n=7 -> '13'
 '''
-# fibonacci test
-# def test_one():
-#     actual= fibonacci(1)
-#     excepted= 1
-#     assert actual == excepted
+#fibonacci test
+def test_one():
+    actual= fibonacci(1)
+    excepted= 1
+    assert actual == excepted
     
-# def test_two():
-#     actual= fibonacci(0)
-#     excepted= 0
-#     assert actual == excepted
+def test_two():
+    actual= fibonacci(0)
+    excepted= 0
+    assert actual == excepted
     
-# def test_three():
-#       actual= fibonacci(3)
-#       excepted= 2
-#       assert actual == excepted
+def test_three():
+      actual= fibonacci(3)
+      excepted= 2
+      assert actual == excepted
       
-# def test_four():
-#       actual= fibonacci(4)
-#       excepted= 3
-#       assert actual == excepted
+def test_four():
+      actual= fibonacci(4)
+      excepted= 3
+      assert actual == excepted
       
-# def test_five():
-#       actual= fibonacci(5)
-#       excepted= 5
-#       assert actual == excepted
+def test_five():
+      actual= fibonacci(5)
+      excepted= 5
+      assert actual == excepted
       
-# def test_six():
-#       actual= fibonacci(6)
-#       excepted= 8
-#       assert actual == excepted
+def test_six():
+      actual= fibonacci(6)
+      excepted= 8
+      assert actual == excepted
       
-# def test_string():
-#       actual= fibonacci("7")
-#       excepted= "please enter a number"
-#       assert actual == excepted
+def test_string():
+      actual= fibonacci("7")
+      excepted= "please enter a number"
+      assert actual == excepted
     
     
-# lucus test 
-# def test_one():
-#       actual= lucus(0)
-#       excepted= 2
-#       assert actual == excepted
+#lucus test 
+def test_one():
+      actual= lucus(0)
+      excepted= 2
+      assert actual == excepted
       
-# def test_two():
-#       actual= lucus(1)
-#       excepted= 1
-#       assert actual == excepted
+def test_two():
+      actual= lucus(1)
+      excepted= 1
+      assert actual == excepted
       
-# def test_three():
-#       actual= lucus(2)
-#       excepted= 3
-#       assert actual == excepted
+def test_three():
+      actual= lucus(2)
+      excepted= 3
+      assert actual == excepted
       
-# def test_string():
-#       actual= lucus("4")
-#       excepted= "please enter a number"
-#       assert actual == excepted 
+def test_string():
+      actual= lucus("4")
+      excepted= "please enter a number"
+      assert actual == excepted 
 
 
 # sum test
