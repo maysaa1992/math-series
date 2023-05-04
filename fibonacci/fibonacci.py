e9c6edd9bdd3a4238898377d0d92082c708f1ef9
maysaa1992 <batainehmaysaa@gmail.com>
2023-05-04 08:58:20 +0000

    update 1 lab2

--- fibonacci/fibonacci.py
+++
@@ -1,8 +0,0 @@
-def fibonacci(n):
-      if type (n) != int:
-        return "please enter a number"
-      if n==1 or n==0:
-        return n
-      else:
-        return fibonacci(n-1)+fibonacci(n-2)
-# print(fibonacci(8))
